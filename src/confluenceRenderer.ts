a7dfa86ad8bf2ee0638748e1e19d3778ab0efc3d
transnano <transnano.jp@gmail.com>
2021-05-09 00:57:48 +0000

    🔥 Remove dt file and revert code.

--- src/confluenceRenderer.ts
+++ src/confluenceRenderer.ts
@@ -21,8 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-// import escapeStringRegexp from "escape-string-regexp";
-import escapeStringRegexp = require('escape-string-regexp');
+import escapeStringRegexp from "escape-string-regexp";
 import { Renderer, Slugger } from "marked";
 
 import {
